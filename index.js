7af8f5b04fdd37f5354c2a7793dd85e7366d2069
Josef Benninghoff <jsbenning4@gmail.com>
2019-09-27 02:00:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 const baseURL = 'https://api.github.com';
-const user = '<YOUR_USERNAME>';
+const baseURL = 'https://api.github.com';
+const user = 'jsbenning';
 
 function getToken() {
+  return 'df37d75097c4d706ccf5423ad3d8481c821ef8ff';
   //change to your token to run in browser, but set
   //back to '' before committing so all tests pass
-  return '';
+  //return '';
 }
 
 function forkRepo() {
   const repo = 'learn-co-curriculum/js-ajax-fetch-lab';
-  //use fetch to fork it!
+  let token = getToken();
+  fetch('https://github.com/' + repo, {
+    headers: {
+      Authorization: `token ${token}`
+    }
+  })
+  .then(res => console.log(res));
 }
 
 function showResults(json) {
